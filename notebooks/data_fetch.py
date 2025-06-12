b32667c263b91f3e6e9e29d75060fe27f6ab3032
nooreen <nooreenamirjan@gmail.com>
2025-06-12 23:28:12 +0200

    testing git command

--- notebooks/data_fetch.py
+++ notebooks/data_fetch.py
@@ -1,45 +1,47 @@
 import pandas as pd
 import os
 from dotenv import load_dotenv
+ghu
 from psycopg import connect
 
 # Load environment variables
 load_dotenv()
 
-# get url
-db_url = os.getenv("DATABASE_URL")
+def fetch_data():
+    # get url
+    db_url = os.getenv("DATABASE_URL")
 
-print(db_url)
+    print(db_url)
 
-# with just url  us connect
-conn = connect(db_url)
-print("Connected to the database")
+    # with just url  us connect
+    conn = connect(db_url)
+    print("Connected to the database")
 
-# Connect and list all table names
-query = """
-    SELECT table_schema, table_name
-    FROM information_schema.tables
-    WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema')
-    ORDER BY table_schema, table_name;
-    """
+    # Connect and list all table names
+    query = """
+        SELECT table_schema, table_name
+        FROM information_schema.tables
+        WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema')
+        ORDER BY table_schema, table_name;
+        """
 
-all_tables = pd.read_sql(query, conn)
+    all_tables = pd.read_sql(query, conn)
 
-# Connect and list all table names
-query = """
-    SELECT *
-    FROM pos_customers
-    """
+    # Connect and list all table names
+    query = """
+        SELECT *
+        FROM pos_customers
+        """
 
-customers_data = pd.read_sql(query, conn)
-customers_data.to_csv("../outputs/customers_data.csv", index=False)
-print("Data exported to CSV files successfully.")
+    customers_data = pd.read_sql(query, conn)
+    customers_data.to_csv("../outputs/customers_data.csv", index=False)
+    print("Data exported to CSV files successfully.")
 
-query = """
-    SELECT *
-    FROM pos_orders
-    """
+    query = """
+        SELECT *
+        FROM pos_orders
+        """
 
-orders_data = pd.read_sql(query, conn)
-orders_data.to_csv("../outputs/orders_data.csv", index=False)
-print("Data exported to CSV files successfully.")
+    orders_data = pd.read_sql(query, conn)
+    orders_data.to_csv("../outputs/orders_data.csv", index=False)
+    print("Data exported to CSV files successfully.")
--- notebooks/main.py
+++ notebooks/main.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import os
-output_folder = '../output'
-file_path = os.path.join(output_folder, 'customer.csv')  
-data = pd.read_csv(file_path)
-import data_fetch 
+from data_fetch import fetch_data
+
+# check in data file for the occurance of the file if not fetch it
+try:
+    customer_data = pd.read_csv("../outputs/customers_data.csv")
+    orders = pd.read_csv("../outputs/orders_data.csv")
+    print("Data files already exist. Skipping fetch.")
+except Exception as e:
+    print(f"How exceptional! {e}")
+    fetch_data()
+    customer_data = pd.read_csv("../outputs/customers_data.csv")
+    orders = pd.read_csv("../outputs/orders_data.csv")
+    print("Data inputed after fetching from DB.")
+
+
+add
